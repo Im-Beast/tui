099573d9f5e97980529e3c7b52d546fb2942a7f5
Im-Beast <franik.mateusz@gmail.com>
2024-09-04 17:44:34 +0200

    fix: export `Suspense` component

--- src/components/mod.ts
+++ src/components/mod.ts
@@ -2,4 +2,5 @@ export * from "./Button.ts";
 export * from "./ProgressBar.ts";
 export * from "./ScrollView.ts";
 export * from "./Spinner.ts";
+export * from "./Suspense.ts";
 export * from "./TextBox.ts";
