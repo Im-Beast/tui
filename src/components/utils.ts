9e2687c81d7a1b95d28256da09565c019ee0dfe3
Im-Beast <franik.mateusz@gmail.com>
2024-09-06 20:04:57 +0200

    fix: `BoundingRectangle` type in `intersects` util method

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,6 +1,6 @@
 import type { BoundingRectangle } from "@tui/nice";
 
-export function intersects(x: number, y: number, rectangle?: BoundingRectangle): boolean {
+export function intersects(x: number, y: number, rectangle: BoundingRectangle | null): boolean {
   if (!rectangle) return false;
   return (
     x > rectangle.left && x <= rectangle.left + rectangle.width &&
